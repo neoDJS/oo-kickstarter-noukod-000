fd2037c345248b2f8e0d06f1a86e4ce1e04ef9cb
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-10-22 14:08:11 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,9 +1,13 @@
 class Backer
-  attr_accessor :name
+  attr_accessor :name 
+  attr_reader :backers
   def initialize(name="")
     @name = name
+    backers = []
   end
 
   def add_baker(bake)
+    raise MismatchTypeError if bake.class != Baker
+    
   end
 end
